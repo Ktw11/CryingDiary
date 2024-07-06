579d64ab64d8d14d6f00d0b65d2d8d01d6866f7b
ktw11 <ktw1897@naver.com>
2024-07-06 18:08:05 +0900

    [Auth] rename FirebaseAuthManager

---
+++ CryingDiary/Authentication/AuthManagable.swift
@@ -0,0 +1,12 @@
+//
+//  AuthManagable.swift
+//  CryingDiary
+//
+//  Created by 공태웅 on 6/23/24.
+//
+
+import Foundation
+
+protocol AuthManagable {
+    var currentUserName: String? { get }
+}
---
+++ CryingDiary/Authentication/FirebaseAuthManager.swift
@@ -0,0 +1,38 @@
+//
+//  FirebaseAuthManager.swift
+//  CryingDiary
+//
+//  Created by 공태웅 on 6/23/24.
+//
+
+import Foundation
+import FirebaseAuth
+
+@Observable
+final class FirebaseAuthManager: AuthManagable {
+    // MARK: Properties
+    
+    // TODO: - User 정보를 담은 객체를 가지도록 수정 필요
+    private(set) var currentUserName: String?
+    
+    // TODO: - Auth 객체 주입해주기
+    private let auth: Auth = Auth.auth()
+    
+    init() {
+        setUpStateListener()
+    }
+}
+
+private extension FirebaseAuthManager {
+    func setUpStateListener() {
+        auth.addStateDidChangeListener { [weak self] auth, firebaseUser in
+            self?.setUserName(to: firebaseUser?.displayName)
+        }
+    }
+    
+    func setUserName(to name: String?) {
+        Task { @MainActor [weak self] in
+            self?.currentUserName = name
+        }
+    }
+}
