88a196f73c9cfb581f5caf79b9732a1c67f7c3c5
ktw11 <ktw1897@naver.com>
2024-07-14 16:45:12 +0900

    [Auth] add Utility function run+defer

---
+++ CryingDiary/Utils/Do+Defer.swift
@@ -0,0 +1,23 @@
+//
+//  Do+Defer.swift
+//  CryingDiary
+//
+//  Created by 공태웅 on 7/14/24.
+//
+
+import Foundation
+
+@discardableResult
+func run<T>(
+    _ operation: () async throws -> T,
+    defer deferredOperation: () async throws -> Void
+) async throws -> T {
+    do {
+        let result = try await operation()
+        try await deferredOperation()
+        return result
+    } catch {
+        try await deferredOperation()
+        throw error
+    }
+}
